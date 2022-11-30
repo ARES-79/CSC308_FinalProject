bc26cf11d24744928f9bd1ac238d5924c7a24b15
archie1802 <jonesarchie37@gmail.com>
2022-11-29 23:15:15 -0800

    temp changes

--- src/CustomTextArea.java
+++ src/CustomTextArea.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,24 +20,34 @@ public class CustomTextArea extends JTextArea implements MyObserver {
         String[] text = this.getText().split("class", -1);
         for (String s : Arrays.copyOfRange(text, 1, text.length)) {
             ArrayList<String> splitClass = (ArrayList<String>) Arrays.stream(s.strip()
-                            .replace("\n", "").split(" "))
+                            .replace("\n", "").split("\\W+"))
                     .collect(Collectors.toList());
-            System.out.println(s.strip().replace("\n", ""));
-            System.out.println(splitClass);
             String boxName = splitClass.get(0);
             UMLComponent box = Blackboard.getBlackboard().getBoxList().stream().filter(b -> b.getName().equals(boxName))
                     .findFirst().orElse(new Box(boxName, 100, 100));
-            int i = 0;
+            int i = 1;
             while (i < splitClass.size()) {
-//                System.out.println(splitClass[i]);
-//                if (splitClass[i + 1].equals("(") && !splitClass[i].equals("method")) {
-//                    classMethods.add(splitClass[i]);
-//                }
+                if(splitClass.get(i).equals("extends")){
+                    i = getConnections("extends", splitClass, i, s, boxName);
+                }
+                else if((splitClass.get(i).equals("implements"))){
+                    i = getConnections("implements", splitClass, i, s, boxName);
+                }
                 i++;
             }
 //            for (String method : classMethods) {
 //                System.out.println(method);
 //            }
+//            class test1 extends test2 {
+//                hello(){
+//                }
+//                method(){
+//                    dumby
+//                }
+//                aaa
+//                        bbb
+//            }
+
 
 
             if (!Blackboard.getBlackboard().getBoxList().contains(box)) {
@@ -52,5 +63,20 @@ public class CustomTextArea extends JTextArea implements MyObserver {
         this.setText("");
         this.parseClasses(Blackboard.getBlackboard().getBoxList());
 
+    }
+
+    private int getConnections(String connectionType, ArrayList<String> splitClass, int i, String s, String boxName){
+        i++;
+        String currentWord = splitClass.get(i);
+        int currentWordIndex = s.indexOf(currentWord);
+        char lastChar = s.charAt(currentWordIndex + currentWord.length() + 1);
+        while(lastChar != '{' && !currentWord.equals("extends") && !currentWord.equals("implements")){
+            System.out.println(boxName + " " + connectionType + " " + currentWord);
+            i++;
+            currentWord = splitClass.get(i);
+            currentWordIndex = s.indexOf(currentWord);
+            lastChar = s.charAt(currentWordIndex + currentWord.length() + 1);
+        }
+        return i;
     }
 }
