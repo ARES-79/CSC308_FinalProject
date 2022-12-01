d9290dca856519fd2251be6fc8c3470478dcb57f
jluna28 <jluna28@calpoly.edu>
2022-11-30 16:06:04 -0800

    documentation

--- src/ConnectionType.java
+++ src/ConnectionType.java
@@ -1,3 +1,10 @@
+/**
+ * Final Project
+ * @author Jamie Luna, Andrew Estrada
+ * @version 1.0
+ * ConnectionType - hold all possible types of connections that can be
+ * drawn/used to connect two boxes
+ */
 public enum ConnectionType {
     ASSOCIATION,
     INHERITANCE,
