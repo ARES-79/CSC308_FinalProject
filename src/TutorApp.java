6f657e6d25e219f1925063085c286e12ed468ac2
Andrew Estrada <a.r.es13trada@gmail.com>
2023-02-15 21:20:42 -0800

    Attempted another version of the transition from MenuScreen to Problem Screen

---
+++ src/TutorApp.java
@@ -0,0 +1,175 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Final Project
+ * @author Andrew Estrada
+ * @version 1.0
+ * View for the Menu for different types of tutoring subjects
+ */
+public class TutorApp extends JFrame {
+
+    private final String username;
+    private JPanel screenPanel;
+    private JMenu file;
+    private TutorController controller = new TutorController(this);
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public JPanel getScreenPanel() {
+        return screenPanel;
+    }
+
+    public void setScreenPanel(JPanel screenPanel) {
+        this.screenPanel = screenPanel;
+    }
+
+    /**
+     * creates a new Menu window and allows it to be seen and closed properly.
+     * args[0] : username of user
+     * args[1] : password of user
+     */
+    public static void main(String[]args){
+        System.out.println("Username: " + args[0] + "    Password: " + args[1]);
+        TutorApp menu = new TutorApp(args[0]);
+        menu.setSize(800,600);
+        menu.setVisible(true);
+        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * Resets the FileMenu to options available for the Menu Window
+     */
+    public void resetFileMenu(){
+        //menu
+        if (file != null){
+            file.removeAll();
+        } else{
+            file = new JMenu("File");
+        }
+        JMenuItem progress = new JMenuItem("View Progress");
+        JMenuItem logOut = new JMenuItem("Log Out");
+
+        file.add(progress);
+        file.add(logOut);
+
+        progress.addActionListener(controller);
+        logOut.addActionListener(controller);
+
+        revalidate();
+        repaint();
+//        if (menuBar != null){
+//            remove(menuBar);
+//            menuBar.removeAll();
+//        } else {
+//            menuBar = new JMenuBar();
+//        }
+//        JMenu file = new JMenu("File");
+//        JMenu help = new JMenu("Help");
+//
+//        JMenuItem progress = new JMenuItem("View Progress");
+//        JMenuItem logOut = new JMenuItem("Log Out");
+//        JMenuItem about = new JMenuItem("About");
+//        setJMenuBar(menuBar);
+//
+//        menuBar.add(file);
+//        menuBar.add(help);
+//        file.add(progress);
+//        file.add(logOut);
+//        help.add(about);
+//
+//        progress.addActionListener(this);
+//        logOut.addActionListener(this);
+//        about.addActionListener(this);
+//
+//        revalidate();
+//        repaint();
+    }
+
+    /**
+     * Sets the FileMenu to options available for Problem Windows
+     */
+    public void alternativeFileMenu(){
+        //menu
+        if (file != null){
+            file.removeAll();
+        } else{
+            file = new JMenu("File");
+        }
+        JMenuItem progress = new JMenuItem("View Progress");
+        JMenuItem backToMenu = new JMenuItem("Back To Menu");
+        JMenuItem logOut = new JMenuItem("Log Out");
+
+        file.add(progress);
+        file.add(backToMenu);
+        file.add(logOut);
+
+        progress.addActionListener(controller);
+        backToMenu.addActionListener(controller);
+        logOut.addActionListener(controller);
+
+        revalidate();
+        repaint();
+
+//        if (menuBar != null){
+//            remove(menuBar);
+//            menuBar.removeAll();
+//        } else {
+//            menuBar = new JMenuBar();
+//        }
+//        JMenu file = new JMenu("File");
+//        JMenu help = new JMenu("Help");
+//
+//        JMenuItem progress = new JMenuItem("View Progress");
+//        JMenuItem backToMenu = new JMenuItem("Back To Menu");
+//        JMenuItem logOut = new JMenuItem("Log Out");
+//        JMenuItem about = new JMenuItem("About");
+//        setJMenuBar(menuBar);
+//
+//        menuBar.add(file);
+//        menuBar.add(help);
+//        file.add(progress);
+//        file.add(backToMenu);
+//        file.add(logOut);
+//        help.add(about);
+//
+//        progress.addActionListener(this);
+//        backToMenu.addActionListener(this);
+//        logOut.addActionListener(this);
+//        about.addActionListener(this);
+//
+//        revalidate();
+//        repaint();
+    }
+
+    /**
+     * Constructor
+     */
+    public TutorApp(String username) {
+        super("My UML Tutor Menu");
+        this.username  = username;
+
+        //menu
+        JMenuBar menuBar = new JMenuBar();
+        resetFileMenu();
+        JMenu help = new JMenu("Help");
+        JMenuItem about = new JMenuItem("About");
+        help.add(about);
+        setJMenuBar(menuBar);
+
+        menuBar.add(file);
+        menuBar.add(help);
+
+        screenPanel = new MenuPanel(username, controller);
+        add(screenPanel);
+
+        //actionListeners
+        about.addActionListener(controller);
+
+    }
+
+}
---
+++ src/TutorController.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TutorController implements ActionListener {
+
+    TutorApp host;
+
+    public TutorController(TutorApp host){
+        this.host = host;
+    }
+
+    /**
+     * Gives functionality to the help menu
+     * @param e the event notifying that an on screen element has been selected
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getActionCommand());
+        switch(e.getActionCommand()){
+            case ("About") -> {
+                JOptionPane.showMessageDialog(host,
+                        "Authors: \n\t- Andrew Estrada \n\t- Jamie Luna \n\t- Archie Jones\n\t- Mitashi Parikh",
+                        "About",
+                        JOptionPane.INFORMATION_MESSAGE);}
+            case ("Code --> UML") -> {
+                host.setVisible(false);
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                new CodeToUMLWindow(host);
+            }
+            case ("Code --> Metrics") -> {
+                host.setVisible(false);
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                new CodeToMetricsWindow(host);
+            }
+            case ("UML --> Code") ->{
+                host.alternativeFileMenu();
+                host.remove(host.getScreenPanel());
+                host.setScreenPanel(new TempAfterMenuPageV3());
+                host.add(host.getScreenPanel());
+                host.revalidate();
+                host.repaint();
+            }
+            case ("View Progress") -> {
+                //
+            }
+            case ("Back To Menu") -> {
+                host.resetFileMenu();
+                host.remove(host.getScreenPanel());
+                host.setScreenPanel(new MenuPanel(host.getUsername(), this));
+                host.add(host.getScreenPanel());
+                host.revalidate();
+                host.repaint();
+            }
+            case ("Log Out") -> {
+                TempSignInWindow.main(null);
+                host.dispose();
+            }
+
+        }
+
+    }
+}
